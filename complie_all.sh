37fec8ac4b784ce2d4a85a9ae6088023f6e11131
Musa Khan <tamzid834@gmail.com>
2016-08-25 01:28:20 +0600

    compile_all.sh added

---
+++ complie_all.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+
+# Compiles all the header and cpp files is given director using g++
+
+
+# if one file is given ,then thats the output
+
+
+output_file_name='output'
+
+if(( $# == 1 ))
+	then
+	    output_file_name = $1
+
+else
+	    output_file_name="output"
+fi
+
+
+
+
+files=$(ls)
+
+h_cpp_files=''
+for file in $files
+do
+	if [[ $file == *.cpp ]] || [[ $file == *.h ]]
+		then
+		    
+		     h_cpp_files="$h_cpp_files  $file " 
+	fi
+done
+
+g++ $h_cpp_files -o $output_file_name

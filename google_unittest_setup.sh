3fca43ad4602492600eee61b0e22cd3005678ca6
Musa Khan <tamzid834@gmail.com>
2017-01-07 19:43:41 +0600

    google_unittest_setup.shis added.

---
+++ google_unittest_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget https://github.com/google/googletest/archive/release-1.7.0.tar.gz
+
+tar xf release-1.7.0.tar.gz
+cd googletest-release-1.7.0
+cmake -DBUILD_SHARED_LIBS=ON .
+make
+
+
+sudo cp -a include/gtest /usr/include
+sudo cp -a libgtest_main.so libgtest.so /usr/lib/

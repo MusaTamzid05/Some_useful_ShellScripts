2c84d279bbe3e4645ff2ed8376b94dab18e03710
Musa Khan <tamzid834@gmail.com>
2017-02-21 08:07:15 +0600

    compile_all_3.sh is added.

---
+++ compile_all_3.sh
@@ -0,0 +1,48 @@
+
+
+TRUE=0
+FALSE=1
+
+function is_h_cpp {
+
+    file=$1
+
+    if [[ $file == *.h ]] || [[ $file == *.cpp ]] || [[ $file == *.hpp ]]
+    then
+        return $TRUE
+    else
+        return $FALSE
+    fi
+
+
+}
+
+complile_file=""
+
+function create_structure {
+
+    files=$(ls $1)
+
+    for file in $files
+    do
+
+        current="$1/$file"
+
+        if [ -d $current ]
+        then
+            create_structure $current
+
+        elif is_h_cpp $current
+        then
+            complile_file="$complile_file $current"
+
+        fi
+
+    done
+}
+
+dir=$(pwd)
+create_structure $dir
+echo $complile_file
+
+g++ $complile_file -std=c++11 -o test
